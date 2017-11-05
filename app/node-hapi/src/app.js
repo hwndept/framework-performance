0f276348803350079dfe41345985108956a2c867
Nikolay Shatalov <hwndept@gmail.com>
2017-11-05 22:32:27 +0200

    Add app written on hapi 16.x.x (#11)

---
+++ app/node-hapi/src/app.js
@@ -0,0 +1,33 @@
+const hapi = require('hapi');
+
+const PORT = process.env.NODE_PORT || 3000;
+
+const server = new hapi.Server();
+
+server.connection({
+  port: PORT
+});
+
+server.start((err) => {
+  if (err) {
+    throw err;
+  }
+
+  console.log('Server is running at:', server.info.uri);
+
+  server.route({
+    method: 'GET',
+    path:'/',
+    handler(req, reply) {
+      reply('Hello!');
+    }
+  });
+
+  server.route({
+    method: 'GET',
+    path:'/{name}',
+    handler(req, reply) {
+      reply(`Hello ${req.params.name}!`);
+    }
+  });
+});
---
+++ app/node-hapi/start.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+npm run docker:build
+
+npm run docker:run
