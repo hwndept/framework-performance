c59419ed9e69e33c32c8873bbd55d409ea6460c7
Nikolay Shatalov <hwndept@gmail.com>
2024-08-23 02:36:32 +0300

    Adds factorial to node-express

--- app/node-express/src/app.js
+++ app/node-express/src/app.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const app = express();
 
+const factorial = require('../../../src/node/factorial');
+
 const PORT = process.env.NODE_PORT || 3000;
 
 app.get('/hello', (req, res) => res.send('Hello!'));
 app.get('/hello/:name', (req, res) => res.send(`Hello ${req.params.name}!`));
+app.get('/factorial/:num', (req, res) => res.send(factorial(parseInt(req.params.num)).toString()));
 
 const server = app.listen(PORT, () => {
   console.log(`listening ${server.address().address}:${server.address().port}`);
