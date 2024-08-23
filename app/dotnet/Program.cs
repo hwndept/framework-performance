36a3d6ef885bcac471ff1e6790e251e3c5ab833d
Nikolay Shatalov <hwndept@gmail.com>
2024-08-23 03:00:59 +0300

    Adds factorial to dotnet

--- app/dotnet/Program.cs
+++ app/dotnet/Program.cs
@@ -1,9 +1,24 @@
+using System.Globalization;
+
 var builder = WebApplication.CreateBuilder(args);
 builder.WebHost.UseUrls("http://0.0.0.0:3000");
 
 var app = builder.Build();
 
+int factorial(int num)
+{
+    if (num == 0)
+    {
+        return 1;
+    }
+    else
+    {
+        return num * factorial(num - 1);
+    }
+}
+
 app.MapGet("/hello", () => "Hello!");
 app.MapGet("/hello/{name}", (string name) => $"Hello {name}!");
+app.MapGet("/factorial/{num}", (int num) => factorial(num));
 
 app.Run();
